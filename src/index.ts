06b51617ce1bdad274348a9fb0074d51fb8d9c1a
hamlet maharjan <hamletmaharjan@lftechnology.com>
2022-11-15 14:55:38 +0545

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  console.log(req);
+  res.send('Express + TypeScript Server edited');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
