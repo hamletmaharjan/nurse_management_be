5703ac24811a4e95bd9d2cab06b6ea25ce37d5fa
hamlet maharjan <hamletmaharjan@lftechnology.com>
2022-12-22 09:18:53 +0545

    add test to validate image

--- dist/__tests__/imageValidator.test.js
+++ dist/__tests__/imageValidator.test.js
@@ -1,29 +1,46 @@
 "use strict";
-// import fs from "fs";
-// import path from "path";
-// import { NextFunction, Request, Response } from 'express';
-// const {LocalFileData} = require('get-file-object-from-local-path');
-// import imageValidator from '../validators/imageValidator';
-// let imageId: string;
-// let imageURL: string;
-// let mockRequest: Partial<Request>;
-//   let mockResponse: Partial<Response>;
-//   let nextFunction: NextFunction = jest.fn();
-//   beforeEach(() => {
-//       mockRequest = {};
-//       mockResponse = {
-//           json: jest.fn(),
-//         };
-//   });
-// describe("file util tests", () => {
-//   test("upload image to cloudinary", async () => {
-//     // const file = fs.readFileSync(path.join(__dirname, "./images/sample.pdf"), "utf8");
-//     const file = LocalFileData(path.join(__dirname, "./images/sample.pdf"));
-//     console.log('file',file)
-//     function callback(data:any, isSuccess:any) {
-//       expect(data).toBe(Error('Only image files are allowed!'));
-//     }
-//     imageValidator(mockRequest as Request, file, callback);
-//   //   imageURL = (await uploadImageToCloudinary(file, "test")) as string;
-//   });
-// });
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const { LocalFileData } = require('get-file-object-from-local-path');
+const imageValidator_1 = __importDefault(require("../validators/imageValidator"));
+let mockRequest;
+let mockResponse;
+let nextFunction = jest.fn();
+beforeEach(() => {
+    mockRequest = {};
+    mockResponse = {
+        json: jest.fn(),
+    };
+});
+describe("Image validator", () => {
+    test("upload pdf", () => __awaiter(void 0, void 0, void 0, function* () {
+        const file = {
+            originalname: 'sample.pdf'
+        };
+        let expectedError = new Error('Only image files are allowed!');
+        function callback(data, isSuccess) {
+            expect(data).toEqual(expectedError);
+        }
+        (0, imageValidator_1.default)(mockRequest, file, callback);
+    }));
+    test("upload image", () => __awaiter(void 0, void 0, void 0, function* () {
+        const file = {
+            originalname: 'sample.jpg'
+        };
+        function callback(data, isSuccess) {
+            expect(isSuccess).toBe(true);
+        }
+        (0, imageValidator_1.default)(mockRequest, file, callback);
+    }));
+});
--- src/__tests__/imageValidator.test.ts
+++ src/__tests__/imageValidator.test.ts
@@ -1,39 +1,46 @@
-// import fs from "fs";
-// import path from "path";
-// import { NextFunction, Request, Response } from 'express';
+import fs from "fs";
+import path from "path";
+import { NextFunction, Request, Response } from 'express';
 
-// const {LocalFileData} = require('get-file-object-from-local-path');
+const {LocalFileData} = require('get-file-object-from-local-path');
 
-// import imageValidator from '../validators/imageValidator';
+import imageValidator from '../validators/imageValidator';
 
-// let imageId: string;
-// let imageURL: string;
 
-// let mockRequest: Partial<Request>;
-//   let mockResponse: Partial<Response>;
-//   let nextFunction: NextFunction = jest.fn();
+let mockRequest: Partial<Request>;
+  let mockResponse: Partial<Response>;
+  let nextFunction: NextFunction = jest.fn();
 
-//   beforeEach(() => {
-//       mockRequest = {};
-//       mockResponse = {
-//           json: jest.fn(),
-//         };
-//   });
+  beforeEach(() => {
+      mockRequest = {};
+      mockResponse = {
+          json: jest.fn(),
+        };
+  });
 
-// describe("file util tests", () => {
-//   test("upload image to cloudinary", async () => {
-//     // const file = fs.readFileSync(path.join(__dirname, "./images/sample.pdf"), "utf8");
+describe("Image validator", () => {
+  test("upload pdf", async () => {
+    const file = {
+      originalname : 'sample.pdf'
+    }
 
-//     const file = LocalFileData(path.join(__dirname, "./images/sample.pdf"));
-//     console.log('file',file)
+    let expectedError = new Error('Only image files are allowed!');
 
-//     function callback(data:any, isSuccess:any) {
-//       expect(data).toBe(Error('Only image files are allowed!'));
-//     }
-//     imageValidator(mockRequest as Request, file, callback);
+    function callback(data:any, isSuccess:any) {
+      expect(data).toEqual(expectedError);
+    }
+    imageValidator(mockRequest as Request, file, callback);
+  });
 
-//   //   imageURL = (await uploadImageToCloudinary(file, "test")) as string;
+  test("upload image", async () => {
+    const file = {
+      originalname : 'sample.jpg'
+    }
 
-//   });
+    function callback(data:any, isSuccess:any) {
+      expect(isSuccess).toBe(true);
+    }
+    imageValidator(mockRequest as Request, file, callback);
+  });
   
-// });
+});
