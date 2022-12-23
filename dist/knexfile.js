96dc8bffb6361c7500b378365fd4fa8233583f54
hamlet maharjan <hamletmaharjan@lftechnology.com>
2022-12-23 13:11:15 +0545

    also read host and port

--- dist/knexfile.js
+++ dist/knexfile.js
@@ -8,10 +8,13 @@ dotenv_1.default.config();
 exports.default = {
     client: 'pg',
     connection: {
+        host: process.env.HOST || '127.0.0.1',
+        port: process.env.DATABASE_PORT || '5432',
         database: process.env.DATABASE || 'nurse_mgmt',
         user: process.env.USER || 'postgres',
         password: process.env.PASSWORD || 'postgres',
     },
+    // connection: process.env.PG_CONNECTION_STRING,
     pool: {
         min: 2,
         max: 10,
--- src/knexfile.ts
+++ src/knexfile.ts
@@ -5,10 +5,13 @@ dotenv.config();
 export default {
     client: 'pg',
     connection: {
+      host: process.env.HOST || '127.0.0.1',
+      port: process.env.DATABASE_PORT || '5432',
       database: process.env.DATABASE || 'nurse_mgmt',
       user: process.env.USER || 'postgres',
       password: process.env.PASSWORD || 'postgres',
     },
+    // connection: process.env.PG_CONNECTION_STRING,
     pool: {
       min: 2,
       max: 10,
